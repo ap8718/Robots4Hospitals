929a3a9351c9e67fb7f752d8f9da9ff581abf555
UMust99 <jjm4618@ic.ac.uk>
2021-05-12 13:19:27 +0200

    calibration added to account for different light

--- glove_detector.py
+++ glove_detector.py
@@ -15,6 +15,7 @@ from object_detection.utils import visualization_utils as viz_utils
 from object_detection.builders import model_builder
 import cv2 
 import numpy as np
+import time
 
 configs = config_util.get_configs_from_pipeline_file(CONFIG_PATH)
 detection_model = model_builder.build(model_config=configs['model'], is_training=False)
@@ -30,9 +31,114 @@ def detect_fn(image):
     detections = detection_model.postprocess(prediction_dict, shapes)
     return detections
 category_index = label_map_util.create_category_index_from_labelmap(ANNOTATION_PATH+'/label_map.pbtxt')
+
 cap = cv2.VideoCapture(0)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+
+print("calibration will begin in 10s, please show a single hand in a glove")
+
+time.sleep(10)
+
+
+filter_value = 50
+
+while True :
+    
+    ret, frame = cap.read()
+    
+    #cv2.imwrite('img.jpg', frame)
+    frame1 = frame
+    hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
+    
+    lower_blue = np.array([110,filter_value,filter_value])
+    upper_blue = np.array([130,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    res = cv2.bitwise_and(frame1,frame1, mask= mask)
+    filtered_frame = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+    
+    #cv2.imshow('res',res)
+    
+    image_np = np.array(filtered_frame)
+    
+    counter = 0
+    for i in range(height) :
+        for k in range(width) : 
+            pixel_intensity = int(image_np[i][k][0]) + int(image_np[i][k][1]) + int(image_np[i][k][2])
+            if pixel_intensity > 0 : 
+                counter += 1
+    
+    
+    
+    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+    detections = detect_fn(input_tensor)
+    
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+    
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    label_id_offset = 1
+    image_np_with_detections = image_np.copy()
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+                image_np_with_detections,
+                detections['detection_boxes'],
+                detections['detection_classes']+label_id_offset,
+                detections['detection_scores'],
+                category_index,
+                use_normalized_coordinates=True,
+                max_boxes_to_draw=2,
+                min_score_thresh=.7,
+                agnostic_mode=False)
+    
+    #cv2.imwrite('img3.jpg', image_np_with_detections)
+    
+    scores = detections['detection_scores']
+    filtered_hand_score1 = scores[0]
+    
+    if filtered_hand_score1 < 0.7 :
+        print("glove not detected")
+        continue
+    
+    x1 = int(detections['detection_boxes'][0][1] * width)
+    y1 = int(detections['detection_boxes'][0][0] * height)
+    x2 = int(detections['detection_boxes'][0][3] * width)
+    y2 = int(detections['detection_boxes'][0][2] * height)
+    
+    
+    counter2 = 0
+    for k in range(y2 - y1) :
+        for i in range(x2 - x1) : 
+            pixel_intensity = int(image_np[y1 + k][x1 + i][0]) + int(image_np[y1 + k][x1 + i][1]) + int(image_np[y1 + k][x1 + i][2])
+            if pixel_intensity > 0 : 
+                counter2 += 1
+            image_np[y1 + k][x1 + i][0] = 0
+            image_np[y1 + k][x1 + i][1] = 0
+            image_np[y1 + k][x1 + i][2] = 255
+    #cv2.imwrite('img2.jpg', image_np)
+    #print(counter)
+    #print(counter2)
+    #break
+    if counter2 * 2 >= counter :
+        break
+    
+    filter_value += 2
+    if filter_value >= 80 :
+        filter_value -= 2
+        break
+    #print(filter_value)
+    #fraction = counter / (width * height)
+    #print(fraction)
+    #cv2.imshow("Image",cv2.resize(image_np, (800, 600)))
+
+print("calibration completed")
+print(filter_value)
+
 while True: 
     ret, frame = cap.read()
     
@@ -40,7 +146,7 @@ while True:
     frame1 = frame
     
     hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([110,80,80])
+    lower_blue = np.array([110,filter_value,filter_value])
     upper_blue = np.array([130,255,255])
 
     # Threshold the HSV image to get only blue colors

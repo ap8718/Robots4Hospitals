6344a1ac1dc1180e7dc6614cc5d567131dd050cc
UMust99 <jjm4618@ic.ac.uk>
2021-05-12 15:54:45 +0200

    Update glove_detector.py

--- glove_detector.py
+++ glove_detector.py
@@ -39,7 +39,7 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 print("calibration will begin in 10s, please show a single hand in a glove")
 
-time.sleep(10)
+#time.sleep(10)
 
 
 filter_value = 50
@@ -124,7 +124,7 @@ while True :
     #print(counter)
     #print(counter2)
     #break
-    if counter2 * 2 >= counter :
+    if int(counter2 * 1.7) >= counter :
         break
     
     filter_value += 2
@@ -155,38 +155,9 @@ while True:
     filtered_frame = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
     #cv2.imshow('res',res)
     
-    image_np = np.array(filtered_frame)
-    
-    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
-    detections = detect_fn(input_tensor)
-    
-    num_detections = int(detections.pop('num_detections'))
-    detections = {key: value[0, :num_detections].numpy()
-                  for key, value in detections.items()}
-    detections['num_detections'] = num_detections
-    
-    scores = detections['detection_scores']
-    filtered_hand_score1 = scores[0]
-    filtered_hand_score2 = scores[1]
-    
-    
-    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+    filtered_image = np.array(filtered_frame)
 
-    label_id_offset = 1
-    image_np_with_detections = image_np.copy()
-
-    viz_utils.visualize_boxes_and_labels_on_image_array(
-                image_np_with_detections,
-                detections['detection_boxes'],
-                detections['detection_classes']+label_id_offset,
-                detections['detection_scores'],
-                category_index,
-                use_normalized_coordinates=True,
-                max_boxes_to_draw=2,
-                min_score_thresh=.7,
-                agnostic_mode=False)
-    cv2.imshow('filtered_image',  cv2.resize(image_np_with_detections, (800, 600)))
-    
+    cv2.imshow('filtered_image',  cv2.resize(filtered_image, (800, 600)))
     
     
     image_np = np.array(frame)
@@ -202,22 +173,48 @@ while True:
     hand_score1 = scores[0]
     hand_score2 = scores[1]
     
+    x11 = int(detections['detection_boxes'][0][1] * width)
+    y11 = int(detections['detection_boxes'][0][0] * height)
+    x21 = int(detections['detection_boxes'][0][3] * width)
+    y21 = int(detections['detection_boxes'][0][2] * height)
+
+    x12 = int(detections['detection_boxes'][1][1] * width)
+    y12 = int(detections['detection_boxes'][1][0] * height)
+    x22 = int(detections['detection_boxes'][1][3] * width)
+    y22 = int(detections['detection_boxes'][1][2] * height)
+    
+    
+    counter1 = 0
+    
+
+    counter2 = 0
+    
+    
     num_hands = 0
+    num_filt_hands = 0
     
     if hand_score1 > 0.6 :
         num_hands += 1
+        for k in range(y21 - y11) :
+            for i in range(x21 - x11) : 
+                pixel_intensity = int(filtered_image[y11 + k][x11 + i][0]) + int(filtered_image[y11 + k][x11 + i][1]) + int(filtered_image[y11 + k][x11 + i][2])
+                if pixel_intensity > 0 : 
+                    counter1 += 1
+        if counter1 * 10 >= (x21 - x11) * (y21 - y11) * 3 :
+            num_filt_hands += 1
+                    
     if hand_score2 > 0.6 :
         num_hands += 1
+        for k in range(y22 - y12) :
+            for i in range(x22 - x12) : 
+                pixel_intensity = int(filtered_image[y12 + k][x12 + i][0]) + int(filtered_image[y12 + k][x12 + i][1]) + int(filtered_image[y12 + k][x12 + i][2])
+                if pixel_intensity > 0 : 
+                    counter2 += 1
+        if counter2 * 10 >= (x22 - x12) * (y22 - y12) * 3 :
+            num_filt_hands += 1
     
-    num_filt_hands = 0
     
-    if filtered_hand_score1 > 0.7 :
-        num_filt_hands += 1
-    if filtered_hand_score2 > 0.7 :
-        num_filt_hands += 1
     
-    if num_filt_hands > num_hands : 
-        num_filt_hands = num_hands
     
     # detection_classes should be ints.
     detections['detection_classes'] = detections['detection_classes'].astype(np.int64)

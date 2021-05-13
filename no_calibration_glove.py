c59367e2afc92535fc5a63f253c7d32a5f81fab9
UMust99 <jjm4618@ic.ac.uk>
2021-05-13 11:52:47 +0200

    Update no_calibration_glove.py

--- no_calibration_glove.py
+++ no_calibration_glove.py
@@ -15,7 +15,6 @@ from object_detection.utils import visualization_utils as viz_utils
 from object_detection.builders import model_builder
 import cv2 
 import numpy as np
-import time
 
 configs = config_util.get_configs_from_pipeline_file(CONFIG_PATH)
 detection_model = model_builder.build(model_config=configs['model'], is_training=False)
@@ -37,7 +36,6 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 
-print("calibration will begin in 10s, please show a single hand in a glove")
 
 #time.sleep(10)
 

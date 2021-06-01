78c00ec45758ded176da119e3625e90c988e800b
ap8718 <ap8718@ic.ac.uk>
2021-06-01 14:36:37 +0000

    01/06 Donning fully on GPU

--- FinalCode/gpuLoop.py
+++ FinalCode/gpuLoop.py
@@ -8,17 +8,23 @@ import time
 prev_img = np.zeros((60,60))
 img = np.zeros((60,60))
 
+read = False
+
 print("Running loop...")
 while True:
     try:
         # img = np.array(Image.open(r"analysis0.png"))
-        img = cv2.imread(r"analysis0.png")
-        if not (np.all(img == prev_img)):
+        img = cv2.imread(r"imagesFromPepper/analysis0.png")
+        if read and not (np.all(img == prev_img)):
             print("Image recieved!")
             os.system("python3 CompiledModels.py")
             # cm2.main()
             # time.sleep(30)
+            read = False
+        else:
+            read = True
         prev_img = img
+
     except KeyboardInterrupt:
         print('\nInterrupted')
         break

b9adfdbce92b7bfc64006e157624b9bbb1ffb691
Arunansu Patra <arunansu.patra18@imperial.ac.uk>
2021-06-01 15:51:58 +0100

    01/06 Donning fully on GPU

---
+++ FinalCode/donningGPU.py
@@ -0,0 +1,126 @@
+import qi
+import os
+import time
+import takePhotoTest as photo
+import movement2 as move
+import sys
+import argparse
+from datetime import datetime
+
+class HumanGreeter(object):
+    """
+    A simple class to react to face detection events.
+    """
+
+    def __init__(self, app):
+        """0
+        Initialisation of qi framework and event detection.
+        """
+        super(HumanGreeter, self).__init__()
+        app.start()
+        session = app.session
+        # Get the service ALMemory.
+        self.memory = session.service("ALMemory")
+        # Connect the event callback.
+
+        self.subscriber = self.memory.subscriber("WordRecognized")
+        self.subscriber.signal.connect(self.on_human_tracked)
+        # Get the services ALTextToSpeech and ALFaceDetection.
+        self.tts = session.service("ALTextToSpeech")
+        self.face_detection = session.service("ALSpeechRecognition")
+        self.tablet = session.service("ALTabletService")
+        self.awareness = session.service("ALBasicAwareness")
+
+        self.face_detection.pause(True)
+
+        self.face_detection.setVocabulary(["please scan me"],False)
+
+        self.face_detection.pause(False)
+        self.face_detection.subscribe("HumanGreeter")
+        self.got_face = False
+        self.session = app.session
+
+    def on_human_tracked(self, value):
+        """
+        Callback for event FaceDetected.
+        """
+        if value == []:  # empty value when the face disappears
+            self.got_face = False
+        elif not self.got_face:  # only speak the first time a face appears
+            self.got_face = True
+            print value
+
+            if 'please scan me' in value[0] and value[1] > 0.4:
+                self.awareness.pauseAwareness()
+                print(self.awareness.isAwarenessPaused())
+                self.tts.say('Okay')
+
+                t1 = datetime.now()
+                move.main(self.session)
+                photo.main(self.session)
+                t2 = datetime.now()
+
+
+                # print( 'analyzing stuff')
+                # os.system('python3 CompiledModels.py')
+
+                # mask = open('MaskText', 'r')
+                # visor = open('VisorText', 'r')
+                # glove = open('GloveText', 'r')
+                # gown = open('GownText', 'r')
+
+
+                # self.tts.say(visor.read())
+                # time.sleep(0.5)
+                # self.tts.say(mask.read())
+                # print(mask.read())
+                # time.sleep(0.5)
+                # self.tts.say(glove.read())
+                # time.sleep(0.5)
+                # print(glove.read())
+                # self.tts.say(gown.read())
+
+                self.tablet.hideImage()
+
+                self.awareness.resumeAwareness()
+
+            self.got_face = False
+
+
+
+
+
+    def run(self):
+        """
+        Loop on, wait for events until manual interruption.
+        """
+        print "Starting HumanGreeter"
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print "Interrupted by user, stopping HumanGreeter"
+            self.face_detection.unsubscribe("HumanGreeter")
+            #stop
+            sys.exit(0)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", type=str, default="10.0.0.83",
+                        help="Robot IP address. On robot or Local Naoqi: use '127.0.0.1'.")
+    parser.add_argument("--port", type=int, default=9559,
+                        help="Naoqi port number")
+
+    args = parser.parse_args()
+    try:
+        # Initialize qi framework.
+        connection_url = "tcp://" + args.ip + ":" + str(args.port)
+        app = qi.Application(["HumanGreeter", "--qi-url=" + connection_url])
+    except RuntimeError:
+        print ("Can't connect to Naoqi at ip \"" + args.ip + "\" on port " + str(args.port) +".\n"
+               "Please check your script arguments. Run with -h option for help.")
+        sys.exit(1)
+
+    human_greeter = HumanGreeter(app)
+    human_greeter.run()
---
+++ FinalCode/takePhotoTest.py
@@ -0,0 +1,125 @@
+import qi
+import os
+import sys
+import time
+import vision_definitions
+# import cv2
+import almath
+from PIL import Image, ImageEnhance
+# import tablet
+
+IP = "root@8.tcp.ngrok.io"
+PORT = "18897"
+
+
+def main(session):
+    """
+    This is just an example script that shows how images can be accessed
+    through ALVideoDevice in Python.
+    Nothing interesting is done with the images in this example.
+    """
+
+    # Get the service ALVideoDevice.
+
+    #video_service = session.service("ALVideoDevice")
+
+    video_service = session.service("ALVideoDevice")
+
+
+    # Register a Generic Video Module
+    # resolution = vision_definitions.k4VGA
+    resolution = vision_definitions.kVGA
+    colorSpace = vision_definitions.kRGBColorSpace
+    fps = 20
+
+    nameId = video_service.subscribe("python_GVM", resolution, colorSpace, fps)
+    tts = session.service("ALTextToSpeech")
+    tts.say('Please stay still, I am going to take a picture of you')
+
+    bap = session.service('ALBasicAwareness')
+
+    motion_service  = session.service("ALMotion")
+
+    print 'getting images in remote'
+    bap.pauseAwareness()
+    print "getting image 0"
+    motion_service.setStiffnesses("Head", 1.0)
+    names      = "Head"
+
+    angleLists = [0*almath.TO_RAD,-7*almath.TO_RAD]
+
+    motion_service.angleInterpolationWithSpeed(names, angleLists, 0.4)
+
+    time.sleep(1)
+    naoImage = video_service.getImageRemote(nameId)
+
+
+
+
+    # Get the image size and pixel array.
+    imageWidth = naoImage[0]
+    imageHeight = naoImage[1]
+    array = naoImage[6]
+    image_string = str(bytearray(array))
+
+
+    # Create a PIL Image from our pixel array.
+    img = Image.frombytes("RGB", (imageWidth, imageHeight), image_string)
+
+
+    # Save the image.
+    img.save(r"imagesFromPepper/analysis0.png", "PNG")
+
+    #result = merge_images(r"imagesFromPepper/camImage1.png", r"imagesFromPepper/camImage2.png")
+    #result.save(r"imagesFromPepper/camImage.png", "PNG")
+
+    tts.say('Picture taken')
+    # tablet.main(session)
+    bap.resumeAwareness()
+    video_service.unsubscribe(nameId)
+
+    os.system("scp -P " + PORT + " imagesFromPepper/analysis0.png " + IP + ":~/Robots4Hospitals/FinalCode/imagesFromPepper/")
+    time.sleep(30)
+    os.system("scp -r -P " + PORT + " " + IP + ":~/Robots4Hospitals/FinalCode/Results/ .")
+
+    mask = open('Results/MaskText', 'r')
+    visor = open('Results/VisorText', 'r')
+    glove = open('Results/GloveText', 'r')
+    gown = open('Results/GownText', 'r')
+
+
+    tts.say(visor.read())
+    time.sleep(0.5)
+    tts.say(mask.read())
+    print(mask.read())
+    time.sleep(0.5)
+    tts.say(glove.read())
+    time.sleep(0.5)
+    print(glove.read())
+    tts.say(gown.read())
+
+    print("All done!")
+
+
+
+if __name__ == "__main__":
+
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--ip", type=str, default="127.0.0.1",
+    #                     help="Robot IP address. On robot or Local Naoqi: use '127.0.0.1'.")
+    # parser.add_argument("--port", type=int, default=9559,
+    #                     help="Naoqi port number")
+
+    # args = parser.parse_args()
+    ip = "10.0.0.83"
+    port = 9559
+    # tts = ALProxy("ALTextToSpeech", ip, port)
+    # tts.say("Connected")
+    session = qi.Session()
+    try:
+        session.connect("tcp://" + ip + ":" + str(port))
+    except RuntimeError:
+        print ("Can't connect to Naoqi at ip \"" + ip + "\" on port " + str(port) +".\n"
+               "Please check your script arguments. Run with -h option for help.")
+        sys.exit(1)
+    main(session)
